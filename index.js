72f4c27c000627e45c7459741bd30412d0a19d2a
sonnguyenhuu.it <sonnguyenhuu.it@gmail.com>
2025-05-14 10:10:05 +0700

    change port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-const PORT = 8081;
+const PORT = 4370;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.text({ type: "*/*" }));
