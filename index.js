82c8f2027251cca2f39a2ff8085a886cf4a5b5f0
sonnguyenhuu.it <sonnguyenhuu.it@gmail.com>
2025-05-14 10:42:32 +0700

    routes

--- index.js
+++ index.js
@@ -7,9 +7,18 @@ const PORT = 8081;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.text({ type: "*/*" }));
 
+// Cấu hình server để nhận dữ liệu từ iClock
 app.get("/iclock/cdata", (req, res) => {
-  console.log("Dữ liệu nhận được từ thiết bị:");
-  console.log(req);
+  console.log("Dữ liệu nhận được từ thiết bị (iClock):");
+  console.log(req.query); // Hiển thị dữ liệu dạng query parameters
+
+  res.status(200).send("OK");
+});
+
+// Cấu hình server để nhận dữ liệu từ device push
+app.get("/device/push", (req, res) => {
+  console.log("Dữ liệu nhận được từ thiết bị (Push):");
+  console.log(req.query); // Hiển thị dữ liệu dạng query parameters
 
   res.status(200).send("OK");
 });
