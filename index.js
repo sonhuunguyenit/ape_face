429d688a7b9de26e98ecd841ae5351a65e4b8bac
sonnguyenhuu.it <sonnguyenhuu.it@gmail.com>
2025-05-22 10:20:27 +0700

    index

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import express from "express";
-import axios from "axios";
+import crypto from "crypto";
+import fs from "fs";
+
+export const base64 = fs.readFileSync("./image.txt", "utf-8");
 
 const app = express();
 
@@ -11,6 +14,8 @@ app.use(
     },
   })
 );
+
+app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
   const { method, url, query, body, headers, ip } = req;
@@ -36,116 +41,84 @@ function generateSessionID() {
     .join("");
 }
 
-function generateToken(registryCode, serialNumber, sessionID) {
+function generateCookieToken(registryCode, sessionID, serialNumber) {
   const rawString = registryCode + serialNumber + sessionID;
   return crypto.createHash("md5").update(rawString).digest("hex");
 }
 
-// Kiểm tra kết nối thiết bị với server (chạy một lần)
-app.get("/iclock/cdata", (req, res) => {
-  const { SN, pushver } = req.query;
+function registerDevice(serialNumber, res) {
+  const registryCode = generateRegistryCode();
 
-  if (!SN) {
-    return res.status(400).send("Missing SN");
-  }
+  const sessionID = generateSessionID();
 
-  if (registeredDevices[SN]) {
-    const device = registeredDevices[SN];
+  const cookieToken = generateCookieToken(
+    registryCode,
+    sessionID,
+    serialNumber
+  );
 
-    res.send(
-      [
-        "registry=ok",
-        `RegistryCode=${device.registryCode}`,
-        `SessionID=${device.sessionID}`,
-        `PushProtVer=${pushver}`,
-        "TransTables=User Transaction, Attendance",
-        "ErrorDelay=30",
-        "RequestDelay=1",
-        "TransTimes=00:00",
-        "TransInterval=1",
-        "Realtime=1",
-        "TimeoutSec=10",
-      ].join("\n")
-    );
-  } else {
-    res.send("OK");
-  }
-});
+  const device = [
+    `RegistryCode=${registryCode}`,
+    `SessionID=${sessionID}`,
+    "RequestDelay=5",
+    "ErrorDelay=10",
+    "TransTables=User Transaction",
+    "Realtime=1",
+    "TimeoutSec=60",
+  ];
 
-// Device gọi để lấy cấu hình
-app.post("/iclock/registry", (req, res) => {
-  const SN = req.query?.SN;
+  registeredDevices[serialNumber] = device;
 
-  if (!SN) {
-    return res.status(400).send("Missing SN");
-  }
-
-  if (!registeredDevices[SN]) {
-    const registryCode = generateRegistryCode();
-    const sessionID = generateSessionID();
-    const token = generateToken(registryCode, SN, sessionID);
-
-    registeredDevices[SN] = {
-      registryCode,
-      sessionID,
-      token,
-      ...req.body,
-    };
-
-    res.cookie("token", token, {
+  res
+    .cookie("token", cookieToken, {
       httpOnly: true,
       path: "/",
       sameSite: "Strict",
-      maxAge: 1000 * 60 * 60 * 24,
-    });
+      maxAge: 1000 * 60 * 60 * 10000,
+    })
+    .send(["registry=ok", ...device].join("\n"));
+}
 
-    return res.send("");
+// Kiểm tra kết nối thiết bị với server (chạy một lần)
+app.get("/iclock/cdata", (req, res) => {
+  const { SN } = req.query;
+
+  if (!SN) {
+    return res.status(400).send("Missing SN");
+  }
+
+  const device = registeredDevices[SN];
+
+  console.log("registeredDevices", registeredDevices);
+
+  if (device) {
+    console.log("METHOD GET - /iclock/cdata: Đã đăng ký device");
+    res.send(device);
   } else {
+    console.log("METHOD GET - /iclock/cdata: Chưa đăng ký device");
     res.send("OK");
   }
 });
 
-// Đăng ký thiết bị vô server nếu chưa kết nối
-app.get("/iclock/push", (req, res) => {
-  const { SN } = req.query;
-
-  if (!SN) {
-    return res.status(400).send("Missing SN");
-  }
-
-  const device = registeredDevices[SN];
-
-  if (!device) {
-    return res.status(400).send("Can not find device");
-  }
-
-  res.send(
-    [
-      "SessionID=" + (device?.sessionID || ""),
-      "ErrorDelay=30",
-      "RequestDelay=1",
-      "TransTimes=00:00",
-      "TransInterval=1",
-      "Realtime=1",
-      "TimeoutSec=10",
-    ].join("\n")
-  );
-});
-
-// Nhận dữ liệu từ thiết bị gửi về
 app.post("/iclock/cdata", (req, res) => {
-  const { SN } = req.query;
-
-  if (!SN) {
-    return res.status(400).send("Missing SN");
-  }
-
-  const device = registeredDevices[SN];
-
+  const { SN, table } = req.query;
   const parts = req.body.trim().split(/\s+/);
 
-  if (device) {
-    if (req.query.table === "ATTLOG") {
+  console.log(parts);
+
+  if (!SN) {
+    return res.status(400).send("Missing SN");
+  }
+
+  if (table === "options") {
+    const device = registeredDevices[SN];
+
+    console.log("registeredDevices", registeredDevices);
+    if (!device) {
+      return registerDevice(SN, res);
+    }
+  } else {
+    if (table === "ATTLOG") {
       const [userId, datetime, _, event] = parts;
 
       if (userId) {
@@ -158,7 +131,7 @@ app.post("/iclock/cdata", (req, res) => {
       }
     }
 
-    if (req.query.table == "OPERLOG") {
+    if (table == "OPERLOG") {
       const [type] = parts;
 
       if (type == "OPLOG") {
@@ -189,12 +162,12 @@ app.post("/iclock/cdata", (req, res) => {
         );
       }
     }
-  }
 
-  res.send("OK");
+    return res.send("OK");
+  }
 });
 
-// Thiết bị hỏi lệnh từ server (heartbeat)
+// Run CMD (heartbeat)
 app.get("/iclock/getrequest", (req, res) => {
   const { SN, INFO } = req.query;
 
@@ -202,69 +175,60 @@ app.get("/iclock/getrequest", (req, res) => {
     return res.status(400).send("Missing SN");
   }
 
-  if (INFO) {
-    console.log("Data bị thay đổi bởi events");
+  const device = registeredDevices[SN];
+
+  if (device) {
+    if (INFO) {
+      console.log("Chạy CMD");
+
+      // const cmd = `C:223:CONTROL DEVICE 03000000`;
+
+      // const cmd = `C:1002:DATA USER PIN=1234\tName=John Doe\tPassword=1234\tCard=12345678\tGroup=1\tPrivilege=0`;
+
+      // const cmd = `C:1002:DATA DELETE USERINFO PIN=1234`
+
+      // const cmd = `C:12345:DATA UPDATE BIOPHOTO PIN=1\tContent=\r\n`;
+
+      //   const date = new Date();
+      //   const cmd1 = `C:${date.getSeconds()}:DATA USER PIN=3\tName=Son\tPassword=3\tCard=3\tGrp=1\tPri=14\r\n`;
+      //   const cmd2 = `C:${
+      //     date.getSeconds() + 1
+      //   }:DATA UPDATE BIOPHOTO PIN=3\tContent=${base64}\r\n`;
+      //   const cmd = `${cmd1}${cmd2}`;
+
+      //   return res.send(cmd);
+    } else {
+      return res.send("OK");
+    }
+  } else {
+    console.log("Device chưa đăng ký");
+    return registerDevice(SN, res);
   }
-
-  res.send("OK");
 });
 
-//  nhận dữ liệu querydata từ thiết bị
-app.post("/iclock/querydata", (req, res) => {
-  const { SN, type, cmdid, tablename, count, packcnt, packidx } = req.query;
+// Response CMD (heartbeat)
+app.post(
+  "/iclock/devicecmd",
+  express.raw({ type: "*/*" }), // buffer
+  (req, res) => {
+    const { SN } = req.query;
 
-  const dataRecord = req.body;
+    const rawBody = req.body.toString("utf-8");
+    console.log("/devicecmd from:", SN);
+    console.log("Raw body:", rawBody);
 
-  console.log("Received QueryData from device:", SN);
-  console.log("Type:", type);
-  console.log("CmdID:", cmdid);
-  console.log("Table:", tablename);
-  console.log("Count:", count);
-  console.log("Pack Count:", packcnt);
-  console.log("Pack Index:", packidx);
-  console.log("DataRecord:", dataRecord);
-
-  res.status(200).send(`${tablename}=${count}`);
-});
-
-// Endpoint lấy danh sách user (gọi ra thiết bị)
-app.get("/users", async (req, res) => {
-  try {
-    const response = await axios.post(
-      `http://192.168.2.43:4370/iclock/querydata`,
-      null,
-      {
-        params: {
-          SN: "8116244700513",
-          type: "tabledata",
-          tablename: "user",
-          count: 0,
-          packcnt: 1,
-          packidx: 0,
-        },
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
+    const result = {};
+    rawBody.split("&").forEach((pair) => {
+      const [key, value] = pair.split("=");
+      if (key && value !== undefined) {
+        result[key] = decodeURIComponent(value);
       }
-    );
-    console.log("Gọi thủ công");
-    // Parse dữ liệu trả về từ thiết bị
-    const users = response.data.split("\n").map((line) => {
-      const fields = line.trim().split(" ");
-      const user = {};
-      fields.forEach((field) => {
-        const [key, value] = field.split("=");
-        user[key] = value;
-      });
-      return user;
     });
 
-    res.json(users);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Failed to fetch users" });
+    console.log("Parsed:", result);
+    res.send("OK");
   }
-});
+);
 
 app.listen(8080, () => {
   console.log("Server running on port 8080");
